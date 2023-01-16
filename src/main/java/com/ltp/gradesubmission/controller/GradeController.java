2761e40b2a9fe21bdb956f6dd3bd498531b6d30c
markonikolic1712 <nikolic.marko11@gmail.com>
2023-01-16 18:36:22 +0100

    jgg

--- src/main/java/com/ltp/gradesubmission/controller/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/controller/GradeController.java
@@ -1,4 +1,4 @@
-package com.ltp.gradesubmission;
+package com.ltp.gradesubmission.controller;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,27 +12,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ltp.gradesubmission.Constants;
+import com.ltp.gradesubmission.Grade;
+import com.ltp.gradesubmission.repository.GradeRepository;
+import com.ltp.gradesubmission.service.GradeService;
+
 @Controller
 public class GradeController {
 
-    // kreira se lista Grade objekata - ova lista ce se setovati modelu kao atribut i sa modelom ce se poslati na view
-    // ova lista ce se popuniti samo jednom i to kada se inicijalizuje klasa GradeController. Kasnije kada se uradi refresh stranice u browseru pozvace se metoda getGrades() ali klasa GradeController je vec inicijalizovana pa se lista nece ponovo kreirati
-    // kada se pokrene aplikacija instancira se ova klasa i kreira se lista stdentGrades. Kada se uradi submit forme poziva se submitGrade() i grade se dodaje u listu. Taj dodati grade ce ostati u listi dok aplikacija radi - dok postoji instanca klase GradeController postojace i property/lista studentGrades i dodati grase u njoj. 
-    List<Grade> studentGrades = new ArrayList<>();
+    GradeRepository gradeRepo = new GradeRepository();
+    GradeService gradeService =  new GradeService();
 
     @GetMapping("/grades")
     public String getGrades(Model model){
-        model.addAttribute("grades", studentGrades);
+        model.addAttribute("grades", gradeService.getGrades());
         return "grades";
     }
 
     @GetMapping("/")
     public String getForm(Model model, @RequestParam(required = false) String id) {
-
-        int index = getGradeIndex(id);
-
         // getGradeIndex(name) == Constants.NOT_FOUND ? new Grade() : studentGrades.get(getGradeIndex(name)) - proverava se da li student postoji u listi. Ako ne postoji onda se kreira prazan Grade objekat a ako postoji uzima se taj student iz liste (po indeksu studentGrades.get(i))
-        model.addAttribute("grade", index == Constants.NOT_FOUND ? new Grade() : studentGrades.get(index));
+        model.addAttribute("grade", gradeService.getGradeById(id));
         return "form";
     }
 
@@ -44,25 +44,10 @@ public class GradeController {
         // Radi se return "form"; zato sto se salje BindingResult a kada bi se radio rediract BindingResult bi se izgubio.
         if(result.hasErrors()) return "form";
 
-        // prvo se proverava da li student postoji u listi i dodaje se samo ako vec nije u listi
-        int index = getGradeIndex(grade.getId());
-        if(index == Constants.NOT_FOUND) {
-            studentGrades.add(grade);
-        } else {
-            // ako student postoji u listi preko postojeceg se upisuje novi - update-ovani
-            studentGrades.set(index, grade);
-        }
+        // ovde se poziva metoda iz servisa i prosledjuje se Grade objekat a ta metoda ce proveriti da li tgreba da se doda novi Grade ili treba da se uradi update postojeceg
+        gradeService.submitGrade(grade);
 
         // kada se uradi submit forme radi se redirect na /grades. Kada bi se radio samo return "grades" onda bi bila ucitana ta strana ali podaci ne bi bili poslati u modelu. Kada se radi redirect na /grades poziva se metoda getGrades() u model se dodaje artibut sa listom grade objekata i taj model se salje na view.
         return "redirect:/grades";
-    }
-
-    // poroverava se da li student sa tim imenom postoji u listi
-    public Integer getGradeIndex(String id){
-        for(int i = 0; i < studentGrades.size(); i++){
-            System.out.println("id: " + id + " | " + studentGrades.get(i).getId());
-            if(studentGrades.get(i).getId().equals(id)) return i;
-        }
-        return Constants.NOT_FOUND;
     }
 }
---
+++ src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
@@ -0,0 +1,34 @@
+package com.ltp.gradesubmission.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ltp.gradesubmission.Grade;
+
+public class GradeRepository {
+    
+    // kreira se lista Grade objekata - ova lista ce se setovati modelu kao atribut i sa modelom ce se poslati na view
+    // ova lista ce se popuniti samo jednom i to kada se inicijalizuje klasa GradeController. Kasnije kada se uradi refresh stranice u browseru pozvace se metoda getGrades() ali klasa GradeController je vec inicijalizovana pa se lista nece ponovo kreirati
+    // kada se pokrene aplikacija instancira se ova klasa i kreira se lista stdentGrades. Kada se uradi submit forme poziva se submitGrade() i grade se dodaje u listu. Taj dodati grade ce ostati u listi dok aplikacija radi - dok postoji instanca klase GradeController postojace i property/lista studentGrades i dodati grase u njoj. 
+    private List<Grade> studentGrades = new ArrayList<>();
+
+    // metoda koja na osnovu indeksa iz liste vraca Grade objekat
+    public Grade getGrade(int index){
+        return studentGrades.get(index);
+    }
+
+    // metoda koja vraca listu svih Grade objekata
+    public List<Grade> getGrades(){
+        return studentGrades;
+    }
+
+    // metoda koja dodaje Grade objekat u listu
+    public void addGrade(Grade grade){
+        studentGrades.add(grade);
+    }
+
+    // metoda koja radu update Grade objekta i to tako sto na mesto starog objekta setuje novi. Prosledjuje se index i na mestu tog index-a se upisuje novi Grade objekat - preko starog
+    public void updateGrade(int index, Grade newGrade){
+        studentGrades.set(index, newGrade);
+    }
+}
